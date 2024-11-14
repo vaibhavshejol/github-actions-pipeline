3c72357e5e895dff951222a11fbd8467d5cd4fe3
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-11-14 22:11:51 +0530

    code modified

--- src/main/java/com/amazingcode/in/example/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.amazingcode.in.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.amazingcode.in.example.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
